9be33a3ac8e08e23a357e5c96e545d5af98219b4
Your Name <your.email@example.com>
2025-06-02 14:34:31 +0800

    Fix Tailwind CSS production setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3490f3',
+        secondary: '#0d141c',
+        neutral: {
+          100: '#f0f3f8',
+          200: '#e7edf4',
+          300: '#cedbe8',
+          400: '#8494a5',
+          500: '#49719c',
+        },
+      },
+    },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms')({
+      strategy: 'class',
+    }),
   ],
 } 
